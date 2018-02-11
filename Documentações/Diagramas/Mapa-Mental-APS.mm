194a64c440216296d9904025271b106d92ff3d91
Gabriel Batista <gabriel.texteis@gmail.com>
2018-02-11 12:41:27 -0200

    inicio

---
+++ Documentações/Diagramas/Mapa-Mental-APS.mm
@@ -0,0 +1,202 @@
+<map version="1.0.1">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node COLOR="#000000" CREATED="1501688455948" ID="ID_1077836399" MODIFIED="1502123026990" STYLE="bubble" TEXT="Atividade Pr&#xe1;tica Supervisionada">
+<font NAME="SansSerif" SIZE="20"/>
+<hook NAME="accessories/plugins/AutomaticLayout.properties"/>
+<node COLOR="#0033ff" CREATED="1502123039157" ID="ID_575564515" MODIFIED="1502129611197" POSITION="right" TEXT="Documenta&#xe7;&#xe3;o">
+<edge STYLE="sharp_bezier" WIDTH="8"/>
+<font NAME="SansSerif" SIZE="18"/>
+<node COLOR="#00b439" CREATED="1502123085873" ID="ID_761141020" MODIFIED="1502129649689" TEXT="Capa">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+<icon BUILTIN="button_ok"/>
+<node COLOR="#990000" CREATED="1502123535018" ID="ID_1032722527" MODIFIED="1502123537509" TEXT="Washington Lopse de Sousa">
+<font NAME="SansSerif" SIZE="14"/>
+<icon BUILTIN="male1"/>
+</node>
+</node>
+<node COLOR="#00b439" CREATED="1502123093533" ID="ID_1052191719" MODIFIED="1502129648572" TEXT="&#xcd;ndice">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+<node COLOR="#990000" CREATED="1502123544709" ID="ID_887713114" MODIFIED="1502123548062" TEXT="Washington Lopse de Sousa">
+<font NAME="SansSerif" SIZE="14"/>
+<icon BUILTIN="male1"/>
+</node>
+</node>
+<node COLOR="#00b439" CREATED="1502123067383" ID="ID_1971325742" MODIFIED="1502129649104" TEXT="Introdu&#xe7;&#xe3;o">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+<node COLOR="#990000" CREATED="1502123561019" ID="ID_1373503524" MODIFIED="1502123569018" TEXT="Felipe da Silva Borges Neves">
+<font NAME="SansSerif" SIZE="14"/>
+<icon BUILTIN="male2"/>
+</node>
+</node>
+<node COLOR="#00b439" CREATED="1502123378118" ID="ID_1043859391" MODIFIED="1502129641072" TEXT="Fundamentos das Principais T&#xe9;cnicas Biom&#xe9;tricas">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+<node COLOR="#990000" CREATED="1502123667423" ID="ID_1861879763" MODIFIED="1502129643137" TEXT="Sistemas Biom&#xe9;tricos">
+<font NAME="SansSerif" SIZE="14"/>
+<node COLOR="#111111" CREATED="1502123697402" ID="ID_1073059403" MODIFIED="1502123721921" TEXT="Gabriel de Almeida Batista">
+<icon BUILTIN="male1"/>
+</node>
+<node COLOR="#111111" CREATED="1502123674600" ID="ID_1561159739" MODIFIED="1502123677685" TEXT="Felipe da Silva Borges Neves">
+<icon BUILTIN="male2"/>
+</node>
+<node COLOR="#111111" CREATED="1502123727787" ID="ID_1320412636" MODIFIED="1502123861771" TEXT="Igor Faggion Silveira Santos">
+<icon BUILTIN="male1"/>
+</node>
+<node COLOR="#111111" CREATED="1502123729376" ID="ID_1543552609" MODIFIED="1502123896258" TEXT="Anderson Alves Schinaid">
+<icon BUILTIN="male2"/>
+</node>
+<node COLOR="#111111" CREATED="1502123899133" ID="ID_1946801204" MODIFIED="1502123908459" TEXT="Washington Lopse de Sousa">
+<icon BUILTIN="male1"/>
+</node>
+</node>
+<node COLOR="#990000" CREATED="1502123946095" ID="ID_1230154415" MODIFIED="1502129646745" TEXT="Uso do Sistema Biom&#xe9;trico na Seguran&#xe7;a P&#xfa;blica">
+<font NAME="SansSerif" SIZE="14"/>
+<node COLOR="#111111" CREATED="1502123977296" ID="ID_1139423787" MODIFIED="1502123980495" TEXT="Felipe da Silva Borges Neves">
+<icon BUILTIN="male2"/>
+</node>
+</node>
+<node COLOR="#990000" CREATED="1502123996512" ID="ID_1503419783" MODIFIED="1502129644150" TEXT="Reconhecimento Facial">
+<font NAME="SansSerif" SIZE="14"/>
+<node COLOR="#111111" CREATED="1502123674600" ID="ID_710228643" MODIFIED="1502123677685" TEXT="Felipe da Silva Borges Neves">
+<icon BUILTIN="male2"/>
+</node>
+<node COLOR="#111111" CREATED="1502123727787" ID="ID_299979777" MODIFIED="1502123861771" TEXT="Igor Faggion Silveira Santos">
+<icon BUILTIN="male1"/>
+</node>
+<node COLOR="#111111" CREATED="1502123899133" ID="ID_1447731392" MODIFIED="1502123908459" TEXT="Washington Lopse de Sousa">
+<icon BUILTIN="male1"/>
+</node>
+</node>
+<node COLOR="#990000" CREATED="1502124047183" ID="ID_1346863365" MODIFIED="1502129645181" TEXT="Uso do Reconhecimento Facial na Seguran&#xe7;a P&#xfa;blica">
+<font NAME="SansSerif" SIZE="14"/>
+<node COLOR="#111111" CREATED="1502123899133" ID="ID_1619422368" MODIFIED="1502123908459" TEXT="Washington Lopse de Sousa">
+<icon BUILTIN="male1"/>
+</node>
+</node>
+</node>
+<node COLOR="#00b439" CREATED="1502123402192" ID="ID_443052439" MODIFIED="1502129636262" TEXT="Plano de Desenvolvimento da Aplica&#xe7;&#xe3;o">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+<icon BUILTIN="messagebox_warning"/>
+<node COLOR="#990000" CREATED="1502124094935" ID="ID_1615599165" MODIFIED="1502129638478" TEXT="Modelagem de Dados">
+<font NAME="SansSerif" SIZE="14"/>
+<icon BUILTIN="messagebox_warning"/>
+<node COLOR="#111111" CREATED="1502123697402" ID="ID_90966422" MODIFIED="1502123721921" TEXT="Gabriel de Almeida Batista">
+<icon BUILTIN="male1"/>
+</node>
+</node>
+<node COLOR="#990000" CREATED="1502124156546" ID="ID_648106439" MODIFIED="1502129637932" TEXT="Modelagem do Sistema">
+<font NAME="SansSerif" SIZE="14"/>
+<node COLOR="#111111" CREATED="1502123697402" ID="ID_480476492" MODIFIED="1502123721921" TEXT="Gabriel de Almeida Batista">
+<icon BUILTIN="male1"/>
+</node>
+</node>
+<node COLOR="#990000" CREATED="1502124168303" ID="ID_1035912150" MODIFIED="1502129637212" TEXT="Prot&#xf3;tipos">
+<font NAME="SansSerif" SIZE="14"/>
+<node COLOR="#111111" CREATED="1502123727787" ID="ID_14636294" MODIFIED="1502123861771" TEXT="Igor Faggion Silveira Santos">
+<icon BUILTIN="male1"/>
+</node>
+</node>
+<node COLOR="#990000" CREATED="1502124195634" ID="ID_704926884" MODIFIED="1502129638996" TEXT="Linguagem de Programa&#xe7;&#xe3;o">
+<font NAME="SansSerif" SIZE="14"/>
+<node COLOR="#111111" CREATED="1502123729376" ID="ID_163642651" MODIFIED="1502123896258" TEXT="Anderson Alves Schinaid">
+<icon BUILTIN="male2"/>
+</node>
+</node>
+<node COLOR="#990000" CREATED="1502124211788" ID="ID_1086790585" MODIFIED="1502129639479" TEXT="Conceito Geral do Programa">
+<font NAME="SansSerif" SIZE="14"/>
+<node COLOR="#111111" CREATED="1502123729376" ID="ID_189344719" MODIFIED="1502123896258" TEXT="Anderson Alves Schinaid">
+<icon BUILTIN="male2"/>
+</node>
+</node>
+</node>
+<node COLOR="#00b439" CREATED="1502123423813" ID="ID_114874634" MODIFIED="1502129632994" TEXT="Projeto do Programa">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+<node COLOR="#990000" CREATED="1502124260620" ID="ID_722910566" MODIFIED="1502129617338" TEXT="Interface Gr&#xe1;fica">
+<font NAME="SansSerif" SIZE="14"/>
+<node COLOR="#111111" CREATED="1502123727787" ID="ID_643852720" MODIFIED="1502123861771" TEXT="Igor Faggion Silveira Santos">
+<icon BUILTIN="male1"/>
+</node>
+</node>
+<node COLOR="#990000" CREATED="1502124246837" ID="ID_1952840701" MODIFIED="1502129617809" TEXT="Banco de Dados">
+<font NAME="SansSerif" SIZE="14"/>
+<node COLOR="#111111" CREATED="1502123727787" ID="ID_270122056" MODIFIED="1502123861771" TEXT="Igor Faggion Silveira Santos">
+<icon BUILTIN="male1"/>
+</node>
+</node>
+</node>
+<node COLOR="#00b439" CREATED="1502123440508" ID="ID_704034077" MODIFIED="1502129615634" TEXT="Relat&#xf3;rio com as Linhas de C&#xf3;digo">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+<node COLOR="#990000" CREATED="1502123517891" ID="ID_191764975" MODIFIED="1502123529277" TEXT="Washington Lopse de Sousa">
+<font NAME="SansSerif" SIZE="14"/>
+<icon BUILTIN="male1"/>
+</node>
+</node>
+<node COLOR="#00b439" CREATED="1502123457696" ID="ID_1537467160" MODIFIED="1502129614916" TEXT="Apresenta&#xe7;&#xe3;o do Programa">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+<node COLOR="#990000" CREATED="1502123517891" ID="ID_17804103" MODIFIED="1502123529277" TEXT="Washington Lopse de Sousa">
+<font NAME="SansSerif" SIZE="14"/>
+<icon BUILTIN="male1"/>
+</node>
+</node>
+<node COLOR="#00b439" CREATED="1502123465179" ID="ID_1744452068" MODIFIED="1502129614353" TEXT="Bibliografia">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+<node COLOR="#990000" CREATED="1502123503294" ID="ID_1325698429" MODIFIED="1502123514578" TEXT="Washington Lopse de Sousa">
+<font NAME="SansSerif" SIZE="14"/>
+<icon BUILTIN="male1"/>
+</node>
+</node>
+<node COLOR="#00b439" CREATED="1502123606517" ID="ID_1337443245" MODIFIED="1502129613182" TEXT="Ficha de Atividades Pr&#xe1;ticas Supervisionadas">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+<node COLOR="#990000" CREATED="1502123611782" ID="ID_1955184302" MODIFIED="1502123614020" TEXT="Washington Lopse de Sousa">
+<font NAME="SansSerif" SIZE="14"/>
+<icon BUILTIN="male1"/>
+</node>
+</node>
+</node>
+<node COLOR="#0033ff" CREATED="1502123046023" ID="ID_1085487424" MODIFIED="1502123048728" POSITION="left" TEXT="Programa">
+<edge STYLE="sharp_bezier" WIDTH="8"/>
+<font NAME="SansSerif" SIZE="18"/>
+<node COLOR="#00b439" CREATED="1502130933285" ID="ID_1780136236" MODIFIED="1502130937343" TEXT="Banco de Dados">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+<node COLOR="#990000" CREATED="1502123727787" ID="ID_1063283384" MODIFIED="1502130950666" TEXT="Igor Faggion Silveira Santos">
+<font NAME="SansSerif" SIZE="14"/>
+<icon BUILTIN="male1"/>
+</node>
+</node>
+<node COLOR="#00b439" CREATED="1502131038380" ID="ID_426536067" MODIFIED="1502132278442" TEXT="Identifica&#xe7;&#xe3;o Biom&#xe9;trica">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+<node COLOR="#990000" CREATED="1502123697402" ID="ID_441221808" MODIFIED="1502131065016" TEXT="Gabriel de Almeida Batista">
+<font NAME="SansSerif" SIZE="14"/>
+<icon BUILTIN="male1"/>
+</node>
+<node COLOR="#990000" CREATED="1502123729376" ID="ID_1859409679" MODIFIED="1502131065047" TEXT="Anderson Alves Schinaid">
+<font NAME="SansSerif" SIZE="14"/>
+<icon BUILTIN="male2"/>
+</node>
+</node>
+<node COLOR="#00b439" CREATED="1502131053804" ID="ID_78024575" MODIFIED="1502131057120" TEXT="Autentica&#xe7;&#xe3;o Biom&#xe9;trica">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+<node COLOR="#990000" CREATED="1502123697402" ID="ID_627455991" MODIFIED="1502131063862" TEXT="Gabriel de Almeida Batista">
+<font NAME="SansSerif" SIZE="14"/>
+<icon BUILTIN="male1"/>
+</node>
+<node COLOR="#990000" CREATED="1502123729376" ID="ID_748498082" MODIFIED="1502131063864" TEXT="Anderson Alves Schinaid">
+<font NAME="SansSerif" SIZE="14"/>
+<icon BUILTIN="male2"/>
+</node>
+</node>
+</node>
+</node>
+</map>
